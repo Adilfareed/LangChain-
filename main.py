9b610391d9c8ab56839699f5d4985ac826871b4c
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2025-08-06 20:35:35 +0500

    configured structured output

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import Annotated,Literal
 
 load_dotenv()
 
+# Define output schema using Pydantic
+class Review(BaseModel):
+    summary: Annotated[str,"A brief summary about review"] 
+    sentiment:Annotated[Literal["pos","neg"],"provide sentiment "]
 
+# Initialize the model
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.6)
-question = input("ask gemnai:")
 
-name = llm.invoke(question)
+# Apply structured output
+structured_model = llm.with_structured_output(Review)
 
+# Input prompt
+result = structured_model.invoke("""
+I dont like this product very much, bad quality.
+""")
 
-print(name.content if hasattr(name, "content") else name)
+# Print result
+print(result)
