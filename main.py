4b57a9d420e5add597f5e37fe2e9fc8237da443e
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2025-08-10 20:26:13 +0500

    configured vector db

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-from pydantic import BaseModel
-from typing import Annotated,Literal
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-load_dotenv()
+loader=PyPDFLoader('Introduction_to_AI.pdf')
 
-# Define output schema using Pydantic
-class Review(BaseModel):
-    summary: Annotated[str,"A brief summary about review"] 
-    sentiment:Annotated[Literal["pos","neg"],"provide sentiment "]
+docs=loader.load()
 
-# Initialize the model
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.6)
+spiliter= RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+   
+)
 
-# Apply structured output
-structured_model = llm.with_structured_output(Review)
-
-# Input prompt
-result = structured_model.invoke("""
-I dont like this product very much, bad quality.
-""")
-
-# Print result
-print(result)
+result=spiliter.split_documents(docs)
+print(result[0].page_content)
